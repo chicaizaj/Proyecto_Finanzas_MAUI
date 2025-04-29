2035fa6dee3c898097f79807ddb9adeb2b7a0da5
Mauricio8 <Mauricio8@MAURICIO-DELL>
2025-04-29 14:21:07 -0500

    Agregar archivos de proyecto.

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Proyecto_Finanzas_MAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
---
+++ Views/Vpantallainicio.xaml.cs
@@ -0,0 +1,14 @@
+namespace Proyecto_Finanzas_MAUI.Views;
+
+public partial class Vpantallainicio : ContentPage
+{
+	public Vpantallainicio()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
